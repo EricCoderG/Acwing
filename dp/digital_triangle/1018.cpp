58e2be73f77a56df5ea4ff17470df8bb1d9ad7e8
Ruilin Geng <211250061@smail.nju.edu.cn>
2024-03-09 15:39:39 +0800

    feat: finish 1.1

---
+++ dp/digital_triangle/1018.cpp
@@ -0,0 +1,52 @@
+/*
+http://ybt.ssoier.cn:8088/problem_show.php?pid=1287
+
+5
+1  4  6  8  10 
+2  5  7  15 17 
+6  8  9  18 20 
+10 11 12 19 21 
+20 23 25 29 33
+
+109
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int> > w(n + 1, vector<int>(n + 1));
+    vector<vector<int> > dp(n + 1, vector<int>(n + 1));
+    
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin >> w[i][j];
+        }
+    }
+    dp[1][1] = w[1][1];
+    for (int i = 2; i <= n; ++i) {
+        dp[i][1] = dp[i - 1][1] + w[i][1];
+        dp[1][i] = dp[1][i - 1] + w[1][i];
+    }
+        
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 2; j <= n; ++j) {
+            dp[i][j] = w[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    cout << dp[n][n] << endl;
+}
+
+int main() {
+
+    solve();
+    return 0;
+}
+/*
+    与最大值不同，最小值需要考虑边界情况和特殊处理，可参考1015.cpp
+*/
---
+++ dp/digital_triangle/1027.cpp
@@ -0,0 +1,62 @@
+/*
+http://ybt.ssoier.cn:8088/problem_show.php?pid=1277
+
+8
+2 3 13
+2 6 6
+3 5 7
+4 4 14
+5 2 21
+5 6 4
+6 3 15
+7 2 14
+0 0 0
+
+67
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 15;
+int w[N][N];
+int dp[N * 2][N][N];
+
+void solve() {
+    int n;
+    cin >> n;
+    while (1) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x == 0 && y == 0 && z == 0) break;
+        w[x][y] = z;
+    }
+
+    for (int k = 2; k <= n * n; ++k) {
+        for (int i1 = 1; i1 <= n; ++i1) {
+            for (int i2 = 1; i2 <= n; ++i2) {
+                int j1 = k - i1, j2 = k - i2;
+                if (j1 >= 1 && j1 <= n && j2 >= 1 && j2 <= n) {
+                    int t = w[i1][j1];
+                    if (i1 != i2) t += w[i2][j2];
+                    int &x = dp[k][i1][i2];
+                    x = max(x, dp[k - 1][i1 - 1][i2 - 1] + t);
+                    x = max(x, dp[k - 1][i1][i2 - 1] + t);
+                    x = max(x, dp[k - 1][i1 - 1][i2] + t);
+                    x = max(x, dp[k - 1][i1][i2] + t);
+                }
+            }
+        }
+    }
+
+    cout << dp[n * 2][n][n];
+
+}
+
+int main() {
+
+    solve();
+    return 0;
+}
